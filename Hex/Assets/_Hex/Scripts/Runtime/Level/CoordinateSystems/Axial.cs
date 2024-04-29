6a34cb1e7f2f4dfd896d761b10566db80d5b64fd
Alejandro Diaz <alejandrodiazjllo@gmail.com>
2024-04-28 20:06:44 -0500

    Added Map creation classes

---
+++ Hex/Assets/_Hex/Scripts/Runtime/Level/CoordinateSystems/Axial.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Hex.Runtime.Level.CoordinateSystems
+{
+    public readonly struct Axial
+    {
+        private static readonly float SQRT3 = Mathf.Sqrt(3);
+
+        public int Q { get; }
+        public int R { get; }
+
+        public Axial(int q, int r)
+        {
+            Q = q;
+            R = r;
+        }
+
+        public float GetDistance(Axial other) => (this - other).AxialDistance();
+
+        private float AxialDistance()
+        {
+            return (Mathf.Abs(Q) + Mathf.Abs(Q + R) + Mathf.Abs(R)) * 0.5f;
+        }
+
+        public static Axial operator -(Axial a, Axial b)
+        {
+            return new Axial(a.Q - b.Q, a.R - b.R);
+        }
+
+        public Vector2 GetCartesianPos(float size)
+        {
+            float x = size * (SQRT3 * Q + SQRT3 / 2f * R);
+            float y = size * (3f / 2f * R);
+            return new Vector2(x, y);
+        }
+    }
+}
